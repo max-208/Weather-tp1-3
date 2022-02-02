595e1d3c55aca894721e957bbbed38bddd316a22
WiPhil <philo102002@gmail.com>
2022-02-02 10:34:20 +0100

    meteoville

---
+++ src/js/controllers/controller.js
@@ -0,0 +1,23 @@
+/*
+id : description
+city : texte decrivant le nom de la ville
+weather_icon : icone decrivant le temps actuel
+temperature : texte decrivant la temperature
+temperature_min : texte decrivant la temperature min
+temperature_max : texte decrivant la temperature min
+wind_speed : texte decrivant la vitesse du vent
+humidity : texte decrivant l'humidité
+pressure : texte decrivant la pression
+
+main-container : centre de la page
+*/
+
+
+
+
+function getSource(){
+    return this.http.get(url)
+}
+
+
+
